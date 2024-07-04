75d781b0cd68ac8f8575f6c9ff1be2bf3a5408e0
Matt <mphlamb@gmail.com>
2024-07-04 16:22:28 +0200

    initial commit

---
+++ jwt_auth/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+
+class User(AbstractUser):
+	email = models.CharField(max_length=50, unique=True, validators=[
+		RegexValidator(
+                # validator regex for email
+                regex=r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+                message="Enter a valid email",
+                code="invalid_registration",
+            ),
+	],)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
