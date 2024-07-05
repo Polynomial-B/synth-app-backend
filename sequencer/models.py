e4870ba76b066810f8e009c45da359476a06b610
Matt <mphlamb@gmail.com>
2024-07-05 10:14:46 +0200

    Name added into Sequencer model

--- sequencer/models.py
+++ sequencer/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Sequencer(models.Model):
     def __str__(self):
-        return f'{self.sequencer}'
+        return f'{self.names}'
+    name = models.CharField(max_length=80)
     sequencer = ArrayField(ArrayField(models.IntegerField()))
     
 
